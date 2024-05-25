917f66d0b897c4736a11b6822a74bb4549ba6027
SongMinQQ <107177636+SongMinQQ@users.noreply.github.com>
2024-05-25 15:40:15 +0900

    Refactor: DB구조 변경에 따른 코드 수정 및 디자인

--- src/pages/Project/ProjectMain/ProjectComponent.jsx
+++ src/pages/Project/ProjectMain/ProjectComponent.jsx
@@ -1,20 +1,21 @@
 import { useEffect, useState } from 'react';
 import './ProjectComponent.css';
 import { useNavigate } from 'react-router-dom';
-const ProjectComponent = (props) => {
+const ProjectComponent = ({projects, content}) => {
     const [project,setProject] = useState([]);
     const navigate = useNavigate();
     useEffect(()=>{
-        if(props.project.content && props.project.content.length > 0){
-            let arr = props.project.content.map((pr)=>(
+        console.log(projects.content);
+        if(projects.content && projects.content.length > 0){
+            let arr = projects.content.map((pr)=>(
                 <div onClick={()=>{navigate(`/project/${pr.projectCode}`)}} key={pr.projectCode} className='project-main-project-container'>
                     <div className='project-main-project-title'>{pr.projectName}</div>
-                    <div className='project-main-project-from'>{pr.team.teamName}, {pr.accountName}님이 생성</div>
+                    <div className='project-main-project-from'>{pr.departmentName} / {pr.teamName} 님이 생성</div>
                     <div>
                         <div className='project-main-project-status'>{pr.projectStatus}</div>
                         <div>
                             <div>{pr.projectStart}~{pr.projectEnd}</div>    {/*서버에서 정제*/}
-                            <div>인원 수 : {pr.member.length}</div>
+                            {/* <div>인원 수 : {pr.member.length}</div> */}
                         </div>
                     </div>
                 </div>
@@ -24,7 +25,7 @@ const ProjectComponent = (props) => {
             let arr = (<div>계정에 진행중인 프로젝트가 없습니다.</div>);
             setProject(arr);
         }
-    },[props])
+    },[projects])
 
     return(
         <section style={{}}>
