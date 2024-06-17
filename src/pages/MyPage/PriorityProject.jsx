1d57a9401464c997772c1b1367950ec2a13ccc68
SongMinQQ <107177636+SongMinQQ@users.noreply.github.com>
2024-06-17 19:28:19 +0900

    Feat: 마이페이지에 프로젝트 정보 출력

--- src/pages/MyPage/PriorityProject.jsx
+++ src/pages/MyPage/PriorityProject.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
 
-const PriorityProject = () => {
+const PriorityProject = ({projInfo}) => {
     return (
         <div className='priority_project_area'>
-            
+            <h5 style={{marginTop: "80px"}}>진행중인 프로젝트</h5>
+            <p>프로젝트명 : {projInfo.projectName}</p>
+            <p>시작일 : {projInfo.projectStart}</p>
+            <p>종료일 : {projInfo.projectEnd}</p>
+            <p>프로젝트 진행 상태 : {projInfo.projectStatus}</p>
         </div>
     );
 };
--- src/pages/MyPage/ProjectDone.jsx
+++ src/pages/MyPage/ProjectDone.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const ProjectDone = () => {
     return (
         <div className='project_done_area'>
-            
+            <p style={{marginTop:"80px"}}>완료된 항목이 없습니다.</p>
         </div>
     );
 };
